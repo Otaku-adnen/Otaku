eb09004edf54425964f09ba2d073dc03da16fa7e
Otaku-adnen <adnenhouidi1@gmail.com>
2024-06-18 16:55:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
-﻿function filterAnimes() {
-    const genre = document.getElementById('genre').value;
-    const season = document.getElementById('season').value;
-    const animes = document.querySelectorAll('.anime-list-grid li');
-
-    animes.forEach(anime => {
-        const animeGenre = anime.getAttribute('data-genre');
-        const animeSeason = anime.getAttribute('data-season');
-        
-        
-        if ((genre === 'all' || animeGenre === genre) && (season === 'all' || animeSeason === season)) {
-            anime.style.display = '';
-        } else {
-            anime.style.display = 'none';
+ function toggleSidebar(event) {
+            event.stopPropagation(); 
+            const sidebar = document.querySelector('.sidebar');
+            sidebar.style.display = sidebar.style.display === 'block' ? 'none' : 'block';
         }
-    });
-}
+
