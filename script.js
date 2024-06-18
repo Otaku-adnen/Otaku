653125e8590966c85ea1a1dfd2fc92a4b3603d15
Otaku-adnen <adnenhouidi1@gmail.com>
2024-06-18 12:48:22 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+﻿function filterAnimes() {
+    const genre = document.getElementById('genre').value;
+    const season = document.getElementById('season').value;
+    const animes = document.querySelectorAll('.anime-list-grid li');
+
+    animes.forEach(anime => {
+        const animeGenre = anime.getAttribute('data-genre');
+        const animeSeason = anime.getAttribute('data-season');
+        
+        
+        if ((genre === 'all' || animeGenre === genre) && (season === 'all' || animeSeason === season)) {
+            anime.style.display = '';
+        } else {
+            anime.style.display = 'none';
+        }
+    });
+}
