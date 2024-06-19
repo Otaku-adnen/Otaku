5b2f3751bce54a97460f218c46db6eee8cc3e08e
Otaku-adnen <adnenhouidi1@gmail.com>
2024-06-19 03:23:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
- function toggleSidebar(event) {
-            event.stopPropagation(); 
-            const sidebar = document.querySelector('.sidebar');
-            sidebar.style.display = sidebar.style.display === 'block' ? 'none' : 'block';
-        }
 
